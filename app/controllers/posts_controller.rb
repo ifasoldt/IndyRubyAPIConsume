aa76d30870e8cc305dc7d0526ec9bd9fe6fa0630
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-07-19 21:54:59 -0400

    Initialize API project

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+
+  def index
+    isaiah = Octokit::Client.new(access_token: ENV["github_key"])
+    @indy_ruby_basic_users = isaiah.search_users("language:ruby location:indianapolis", page: params[:page], sort: 'joined', order: 'asc')
+    @pages = (@indy_ruby_basic_users[:total_count]/30.0).ceil
+    @indy_ruby_advanced_users = @indy_ruby_basic_users[:items].map{ |this_user| Octokit.user(this_user.login) }
+    @indy_ruby_users_recent_repos = @indy_ruby_basic_users[:items].map {}
+  end
+
+  def recent_activity
+    @recent_activities = Octokit.user_events(params[:user], sort:'created_at')
+  end
+
+  def repositories
+    repositories_search = Octokit.search_repos("user:#{params[:user]}", sort:'stars', per_page: 5, page: params[:page])
+    @pages = (repositories_search[:total_count]/5.0).ceil
+    @repositories = repositories_search[:items]
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+
+  root 'posts#index'
+  get '/recent_activity/(:user)' => 'posts#recent_activity', as: :recent_activity
+  get '/repositories/(:user)' => 'posts#repositories', as: :repositories
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
